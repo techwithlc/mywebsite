b9e479e3a753139e3c0cecb9c1eaf4da9b434d2e
lawrencechen0921 <show99520show99520@gmail.com>
2025-01-07 04:06:34 +0800

    Update htaccess

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        format: 'es'
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
+});
