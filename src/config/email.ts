a55e871134173bc5dcb9f153196d21bc2ba67087
lawrencechen0921 <show99520show99520@gmail.com>
2025-01-15 02:59:14 +0800

    Fix website issues

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,1 +1,5 @@
- 
+export const EMAIL_CONFIG = {
+  SERVICE_ID: import.meta.env.VITE_EMAILJS_SERVICE_ID as string,
+  TEMPLATE_ID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string,
+  PUBLIC_KEY: import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string,
+}; 
